b193b5df81a7da2ee4a842d608f545261bec8df6
shiva <shivakumar.murki@gmail.com>
2022-07-30 10:47:43 +0530

    changes

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "${var.region}"
-    access_key = "AKIA262M72JNKLRTZMWE"
-    secret_key = "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"
+    #access_key = "AKIA262M72JNKLRTZMWE"
+    #secret_key = "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"
     
 }
 
